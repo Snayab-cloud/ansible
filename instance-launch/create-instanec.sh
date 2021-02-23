27ed87b7b9e00aeee69a655355c5484ad3d710cc
Snayab-cloud <shaiknayab2525@gmail.com>
2021-02-23 14:58:32 +0530

    inv

--- instance-launch/create-instanec.sh
+++ instance-launch/create-instanec.sh
@@ -10,7 +10,7 @@ fi
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
 if [ "$STATE" != "running" ]; then
-aws ec2 run-instances --launch-template LaunchTemplateId=lt-0835ecf0be1ed2f76 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
+aws ec2 run-instances --launch-template LaunchTemplateId=lt-0f1bada4fd8a6ab57 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 sleep 10
 fi
 
